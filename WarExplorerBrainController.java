282e45bb0b36e2a852120feb5967cf746a8e3496
Mossi HASHEMI <mossi.hashemi@gmail.Com>
2017-11-12 23:43:46 +0100

    Add functionality : Picking up the resources by WarExplorer

--- WarExplorerBrainController.java
+++ WarExplorerBrainController.java
@@ -1,6 +1,11 @@
 package feelsgoodman;
 
+import java.util.List;
+
 import edu.warbot.agents.agents.WarExplorer;
+import edu.warbot.agents.enums.WarAgentType;
+import edu.warbot.agents.percepts.WarAgentPercept;
+import edu.warbot.agents.resources.WarFood;
 import edu.warbot.brains.brains.WarExplorerBrain;
 
 public abstract class WarExplorerBrainController extends WarExplorerBrain {
@@ -12,10 +17,30 @@ public abstract class WarExplorerBrainController extends WarExplorerBrain {
 
     @Override
     public String action() {
+		
+    	setDebugString("searching food!");  //Retourne la chaine de caractère qui est affichée par l’agent.
 
-        if (isBlocked())
-            setRandomHeading();
+        if (isBlocked()) {                   //isBlocked () : Boolean =>Retourne vrai si l’agent est bloqué contre un bord, faux sinon.
+            setRandomHeading();            //Change aléatoirement la trajectoire de l’agent.
+            return WarExplorer.ACTION_MOVE;    // -----------------------------------------------------???
+        }
+        
+        List<WarAgentPercept> percepts = getPercepts();
+        for (WarAgentPercept p : percepts) {
+
+	    	if (p.getType() == WarAgentType.WarFood) {
+	    		setDebugString("taking Food");
+	            if (p.getDistance() < WarFood.MAX_DISTANCE_TAKE && !isBagFull()) {
+	                setHeading(p.getAngle());
+	                return WarExplorer.ACTION_TAKE;
+	            } else if (!isBagFull()) {
+	            	setHeading(p.getAngle());
+	                return WarExplorer.ACTION_MOVE;
+	            }
+	        }
+
+        }        
+
         return WarExplorer.ACTION_MOVE;
     }
-
 }
